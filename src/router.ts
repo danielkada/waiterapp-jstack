f8bda9adecc9af94cc97633a128bcaeb0ab06e9f
Kadã <dgames201615@outlook.com>
2023-01-06 23:10:21 -0300

    product listing implementation

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
+import { listProducts } from './app/useCases/products/listProducts';
 
 export const router = Router();
 
@@ -12,9 +13,7 @@ router.get('/categories', listCategories);
 router.post('/categories', createCategory);
 
 // List products
-router.get('/products', (req, res) => {
-  res.send('OK');
-});
+router.get('/products', listProducts);
 
 // Create product
 router.post('/products', (req, res) => {
