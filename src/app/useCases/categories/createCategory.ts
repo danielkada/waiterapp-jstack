3d7faa1a092200d50c3ed43772da57493785fe53
Kadã <dgames201615@outlook.com>
2023-01-06 23:06:32 -0300

    added try/catch and http code

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -4,10 +4,15 @@ import { Category } from '../../models/Category';
 export async function createCategory(req: Request, res: Response) {
   const { name, icon } = req.body;
 
-  const createdCategory = await Category.create({
-    name,
-    icon,
-  });
+  try {
+    const createdCategory = await Category.create({
+      name,
+      icon,
+    });
 
-  return res.json(createdCategory);
+    return res.status(201).json(createdCategory);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -2,7 +2,13 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function listCategories(req: Request, res: Response) {
-  const categories = await Category.find();
+  try {
+    const categories = await Category.find();
 
-  return res.json(categories);
+    return res.status(200).json(categories);
+  } catch(error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+
 }
