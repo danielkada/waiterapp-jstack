e16e3953950010a4d216103ce8deb4c07499288b
Kadã <dgames201615@outlook.com>
2023-01-07 10:16:24 -0300

    serving static files

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 
 import { router } from './router';
 
+import path from 'node:path';
+
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const port = 3001;
 
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
     app.use(express.json());
     app.use(router);
 
