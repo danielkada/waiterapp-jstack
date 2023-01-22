452f60bce029ac6d7542ecc812a9bb9122fc7e0f
Kadã <dgames201615@outlook.com>
2023-01-22 09:43:04 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import mongoose from 'mongoose';
 import { router } from './router';
 
 import path from 'node:path';
+import { cors } from './app/middlewares/cors';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const port = 3001;
 
+    app.use(cors);
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
     app.use(express.json());
     app.use(router);
