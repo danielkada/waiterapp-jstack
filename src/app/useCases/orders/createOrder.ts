830b3fdae298869d0ac65888fa3af5c3feda8cc4
Kadã <dgames201615@outlook.com>
2023-01-22 11:57:49 -0300

    implementing the websockets

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { io } from '../../..';
 import { Order } from '../../models/Order';
 
 export async function createOrder(req: Request, res: Response) {
@@ -9,6 +10,9 @@ export async function createOrder(req: Request, res: Response) {
       table,
       products
     });
+    const orderDetails = await createdOrder.populate('products.product');
+
+    io.emit('orders@new', orderDetails);
 
     res.status(201).json(createdOrder);
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { Server } from 'socket.io';
+
+import http from 'node:http';
+import path from 'node:path';
+
 import { router } from './router';
 
-import path from 'node:path';
 import { cors } from './app/middlewares/cors';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
 
     app.use(cors);
@@ -16,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017')
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
